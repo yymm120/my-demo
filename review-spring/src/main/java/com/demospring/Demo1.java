8ceb6490edaf25d8c7d5c16127fa01f62ee08e3f
lxh <1208401731@qq.com>
2022-06-05 13:47:48 +0800

    Demo1 - Spring - IOC(DI) scope: singleton, prototype

---
+++ review-spring/src/main/java/com/demospring/Demo1.java
@@ -0,0 +1,20 @@
+package com.demospring;
+
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+/**
+ * Demo1: IOC(DI) 控制反转(依赖注入)
+ * 依赖注入和控制反转指的都是一个东西, 将bean的生命交给容器管理
+ *   - javaBean的创建不是我们手动new出来的
+ *   - javaBean的属性也不在代码中set进去的
+ */
+public class Demo1 {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext application = new ClassPathXmlApplicationContext("applicationContext.xml");
+        application.getBean("user"); // singleton单例
+        application.getBean("role"); // prototype原型
+        application.close();
+    }
+}
