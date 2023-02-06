0304bb8ea13517ec53baaeea8ff99a8f401f497f
xinhao <1208401731@qq.com>
2023-02-06 19:29:44 +0800

    fix

--- demofg/demo02-pixel/src/main/java/com/example/Main.java
+++ demofg/demo02-pixel/src/main/java/com/example/Main.java
@@ -4,22 +4,27 @@ import com.facebook.ads.sdk.APIContext;
 import com.facebook.ads.sdk.APINodeList;
 import com.facebook.ads.sdk.AdAccount;
 import com.facebook.ads.sdk.Campaign;
+import com.facebook.ads.sdk.serverside.EventRequest;
 
 public class Main {
     public static final APIContext context = new APIContext(
             "{access-token}",
             "{appsecret}"
     );
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        AdAccount account = new AdAccount("act_{{adaccount-id}}", context);
-        try {
-            APINodeList<Campaign> campaigns = account.getCampaigns().requestAllFields().execute();
-            for(Campaign campaign : campaigns) {
-                System.out.println(campaign.getFieldName());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws Exception {
+        MetaPixelTestDroplet droplet = new MetaPixelTestDroplet();
+
+        droplet.run();
+        // System.out.println("Hello world!");
+        // AdAccount account = new AdAccount("act_{{adaccount-id}}", context);
+        // try {
+        //
+        //     APINodeList<Campaign> campaigns = account.getCampaigns().requestAllFields().execute();
+        //     for(Campaign campaign : campaigns) {
+        //         System.out.println(campaign.getFieldName());
+        //     }
+        // } catch (Exception e) {
+        //     e.printStackTrace();
+        // }
     }
 }
---
+++ demofg/demo02-pixel/src/main/java/com/example/MetaPixelTestDroplet.java
@@ -0,0 +1,82 @@
+package com.example;
+
+import com.facebook.ads.sdk.APIContext;
+import com.facebook.ads.sdk.serverside.*;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class MetaPixelTestDroplet {
+
+    public String result = null;
+    public String accessToken = null;
+    public String pixelId = null;
+    public String json = null;
+
+
+
+
+    private EventRequest getEventRequest(APIContext context, int num) {
+        UserData userData = new UserData()
+                .email(String.format("joe%s@eg.com", num));
+//                .clientIpAddress(clientIpAddress)
+//                .clientUserAgent(clientUserAgent);
+
+        HashMap<String, String> customProperties = new HashMap<>();
+        customProperties.put("custom1", "value2");
+
+        CustomData customData = new CustomData()
+                .currency("usd")
+                .customProperties(customProperties)
+                .value(123.45F);
+
+        Event pageViewEvent = new Event();
+        pageViewEvent.eventName("Purchase")
+                .eventTime(System.currentTimeMillis() / 1000L)
+                .userData(userData)
+                .customData(customData);
+                // .eventSourceUrl("http://jaspers-market.com/product/123")
+                // .actionSource(ActionSource.website);
+
+        EventRequest eventRequest = new EventRequest(pixelId, context);
+        eventRequest.addDataItem(pageViewEvent);
+        eventRequest.setTestEventCode("TEST18648");
+
+        return eventRequest;
+    }
+
+    public void run() throws Exception {
+        accessToken = "EAAKLZA0wFwuwBACOXFWdTzqPFxsVLw9OqdrQyZBUjfNhKHk7OAIFZAFCuhtvEnZCTzuuYvO5xDSLHlZBIYFkgk0PQSvCpcpOsAH2zAsQp20vBIHehV1v3oQMLtCpv1cKZB4jEjlYKRTILlAzK9Rys7QbRx5YkPx9glbcpnN3YFcZCSvFgweeZChsyHN7hNhxuHIZD";
+        pixelId = "944408690055580";
+        if (accessToken == null || pixelId == null) {
+            throw new Exception(String.format("Missing required test config. Got pixel_id: '%s', access_token: '%s'", pixelId, accessToken));
+        }
+        APIContext context = new APIContext(accessToken);
+        context.setLogger(System.out);
+
+        EventRequest asyncRequest = getEventRequest(context, 1);
+
+        System.out.println("request" + new ObjectMapper().configure(JsonGenerator.Feature.IGNORE_UNKNOWN, true).writerWithDefaultPrettyPrinter().writeValueAsString(asyncRequest));
+        final ListenableFuture<EventResponse> requestFuture = asyncRequest.executeAsync();
+        EventResponse asyncResponse = requestFuture.get();
+
+        List<ListenableFuture<EventResponse>> eventFutures = new ArrayList<>();
+        eventFutures.add(getEventRequest(context, 2).executeAsync());
+        eventFutures.add(getEventRequest(context, 3).executeAsync());
+        eventFutures.add(getEventRequest(context, 4).executeAsync());
+        List<EventResponse> asyncResponses = Futures
+                .allAsList(eventFutures)
+                .get();
+       System.out.println(String.format("Multiple Async Requests - OK: %s", asyncResponses));
+        result = new ObjectMapper().writeValueAsString(asyncResponses);
+        wait(200);
+    }
+
+}
+
+
